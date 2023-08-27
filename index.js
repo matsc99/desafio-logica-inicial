c3b0880535b5bad8e37a50d95ec918d147120834
matsc99 <mateus.sousa.cruz@hotmail.com>
2023-08-27 18:24:24 -0300

    melhoria

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ let nomeHeroi = "MatSC"
 if (nivelXP <= 1000){
     console.log(" O Herói " + nomeHeroi + " está no nível Ferro");
 } 
-else if (nivelXP >= 1001 && nivelXP <= 2000){
+else if (1001 <= nivelXP <= 2000){
     console.log(" O Herói " + nomeHeroi + " está no nível Bronze");
 }   
-else if (nivelXP >= 2001 && nivelXP <= 5000){
+else if (2001 <= nivelXP <= 5000){
     console.log(" O Herói " + nomeHeroi + " está no nível Prata");
 } 
-else if (nivelXP >= 3001 && nivelXP <= 7000){
+else if (3001 <= nivelXP <= 7000){
     console.log(" O Herói " + nomeHeroi + " está no nível Ouro");
 } 
-else if (nivelXP >= 7001 && nivelXP <= 8000){
+else if (7001 <= nivelXP <= 8000){
     console.log(" O Herói " + nomeHeroi + " está no nível Platina");
 } 
-else if (nivelXP >= 8001 && nivelXP <= 9000){
+else if (8001 <= nivelXP <= 9000){
     console.log(" O Herói " + nomeHeroi + " está no nível Ascendente");
 } 
-else if (nivelXP >= 9001 && nivelXP <= 10000){
+else if (9001 <= nivelXP <= 10000){
     console.log(" O Herói " + nomeHeroi + " está no nível Imortal");
 }
 else {
