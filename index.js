799e43b7e2d3fe5b46f5e89351c8d6ad1fa60105
matsc99 <mateus.sousa.cruz@hotmail.com>
2023-08-28 13:19:41 -0300

    correção

--- index.js
+++ index.js
@@ -2,27 +2,27 @@
 let nivelXP = 20000
 let nomeHeroi = "MatSC"
 
-if (nivelXP <= 1000){
-    console.log(" O Herói " + nomeHeroi + " está no nível Ferro");
+if (nivelXP <= 1000) {
+    console.log("O Herói " + nomeHeroi + " está no nível Ferro");
 } 
-else if (1001 <= nivelXP <= 2000){
-    console.log(" O Herói " + nomeHeroi + " está no nível Bronze");
+else if (nivelXP >= 1001 && nivelXP <= 2000) {
+    console.log("O Herói " + nomeHeroi + " está no nível Bronze");
 }   
-else if (2001 <= nivelXP <= 5000){
-    console.log(" O Herói " + nomeHeroi + " está no nível Prata");
+else if (nivelXP >= 2001 && nivelXP <= 5000) {
+    console.log("O Herói " + nomeHeroi + " está no nível Prata");
 } 
-else if (3001 <= nivelXP <= 7000){
-    console.log(" O Herói " + nomeHeroi + " está no nível Ouro");
+else if (nivelXP >= 5001 && nivelXP <= 7000) {
+    console.log("O Herói " + nomeHeroi + " está no nível Ouro");
 } 
-else if (7001 <= nivelXP <= 8000){
-    console.log(" O Herói " + nomeHeroi + " está no nível Platina");
+else if (nivelXP >= 7001 && nivelXP <= 8000) {
+    console.log("O Herói " + nomeHeroi + " está no nível Platina");
 } 
-else if (8001 <= nivelXP <= 9000){
-    console.log(" O Herói " + nomeHeroi + " está no nível Ascendente");
+else if (nivelXP >= 8001 && nivelXP <= 9000) {
+    console.log("O Herói " + nomeHeroi + " está no nível Ascendente");
 } 
-else if (9001 <= nivelXP <= 10000){
-    console.log(" O Herói " + nomeHeroi + " está no nível Imortal");
+else if (nivelXP >= 9001 && nivelXP <= 10000) {
+    console.log("O Herói " + nomeHeroi + " está no nível Imortal");
 }
 else {
-    console.log(" O Herói " + nomeHeroi + " está no nível Radiante");
-} 
+    console.log("O Herói " + nomeHeroi + " está no nível radiante");
+}
